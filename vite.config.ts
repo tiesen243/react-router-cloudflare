239b7f645262d101eb256c5b57f2bd74adf43448
tiesen243 <ttien56906@gmail.com>
2026-01-15 14:20:49 +0700

    chore: bump deps

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,18 @@ import { cloudflare } from '@cloudflare/vite-plugin'
 import { reactRouter } from '@react-router/dev/vite'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
+
+import path from 'path'
 
 export default defineConfig({
   plugins: [
     cloudflare({ viteEnvironment: { name: 'ssr' } }),
     tailwindcss(),
     reactRouter(),
-    tsconfigPaths(),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'app'),
+    },
+  },
 })
